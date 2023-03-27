b95c93a153c3135e49e0db249f2f1f1ac4e1515b
unknown <shisthahaneef@gmail.com>
2023-03-27 11:20:36 +1100

    create new time record

--- February2023/February2023/Program.cs
+++ February2023/February2023/Program.cs
@@ -39,3 +39,71 @@ else
 }
 
 
+//create new time record
+
+//navigate to time and material page
+
+IWebElement administrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a/span"));
+administrationDropdown.Click();
+
+IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+tmOption.Click();
+Thread.Sleep(1000);
+
+//click on create new button
+
+IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+createNewButton.Click();
+Thread.Sleep(1000);
+
+//select timeoption from typecode dropdown list
+
+IWebElement typecodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]/span"));
+typecodeDropdown.Click();
+Thread.Sleep(1000);
+
+IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
+timeOption.Click();
+
+//input code into code textbox
+
+IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+codeTextbox.SendKeys("February2023");
+
+//input description into description textbox
+
+IWebElement descriptiontextbox = driver.FindElement(By.Id("Description"));
+descriptiontextbox.SendKeys("February2023");
+
+//input price per unit into price per unit textbox
+
+IWebElement priceTextbox = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+priceTextbox.SendKeys("12");
+
+
+//click on save button
+
+IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+Thread.Sleep(2000);
+
+//check if new time record has been created
+
+IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+goToLastPageButton.Click();
+Thread.Sleep(3000);
+
+IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+if (newCode.Text == "February2023")
+{
+    Console.WriteLine("New Time record created successfully");
+
+}
+else
+{
+    Console.WriteLine("Record Hasn't been created");
+}
+
+
+
